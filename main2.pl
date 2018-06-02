b1273ff12b4f041be5693c758eeadd1c5170d7f2
Javier Ruiz Calle <jarc0212@gmail.com>
2018-06-02 11:11:00 +0200

    Add base case to iniciar predicate

--- main2.pl
+++ main2.pl
@@ -9,8 +9,7 @@ alumno_prode(copado, redondo, sergio, t110040).
 alumno_prode(calle, ruiz, javier, v130126).
 
 cierre(ListaSinRepeticiones, ListaCerrada):- %cierre/2
-    comprobarListaSinRepeticiones(ListaSinRepeticiones),
-    iniciar(ListaSinRepeticiones, ListaCerrada).
+    iniciar(ListaSinRepeticiones, ListaSinRepeticiones, ListaCerrada).
 
 comprobarListaSinRepeticiones([]) :- !.
 comprobarListaSinRepeticiones([X | T]):-
@@ -22,12 +21,14 @@ comprobarRepetidos(eslabon(As, Bs), [eslabon(A, B) | T]):-
     eslabon(As, Bs) \= eslabon(B, A),
     comprobarRepetidos(eslabon(As, Bs), T).
 
-iniciar(ListaSinRepeticiones, ListaCerrada) :-
-    member(eslabon(A, B), ListaSinRepeticiones),
+iniciar(_, [], _).
+iniciar(ListaSinRepeticiones, RecorrerLista, ListaCerrada) :-
+    member(eslabon(A, B), RecorrerLista),
     delete(ListaSinRepeticiones, eslabon(A, B), ListaSinBase),
+    delete(RecorrerLista, eslabon(A,B), RecorrerResto),
     buscar(B, A, ListaSinBase, [eslabon(A, B)], ListaCerrada),
     buscar(A, B, ListaSinBase, [eslabon(A, B)], ListaCerrada),
-    iniciar(ListaSinBase, ListaCerrada).
+    iniciar(ListaSinRepeticiones, RecorrerResto, ListaCerrada).
 
 buscar(_, _, [], _, _).
 buscar(A, B, [eslabon(As, Bs) | T], Acc, ListaCerrada) :-
@@ -35,6 +36,7 @@ buscar(A, B, [eslabon(As, Bs) | T], Acc, ListaCerrada) :-
     delete([As, Bs], B, [Siguiente | _]),
     append(Acc, [eslabon(As, Bs)], NewAcc),
     comprobarCierre(A, Siguiente, NewAcc, ListaCerrada),
+    \+ ground(ListaCerrada),
     buscar(A, Siguiente, T, NewAcc, ListaCerrada).
 buscar(A, B, [eslabon(As, Bs) | T], Acc, ListaCerrada) :-
     \+ member(B, [As, Bs]),
