ed9d1f63f0570862c3592e281be83defe2952c6c
Javier Ruiz Calle <jarc0212@gmail.com>
2018-06-04 15:51:08 +0200

    First try at unique closed links

--- main.pl
+++ main.pl
@@ -5,6 +5,8 @@
 % Ejemplo 2: cierre([eslabon(c, b), eslabon(c, d), eslabon(a, b), eslabon(e, b), eslabon(d, b), eslabon(a, e)], X).
 % Ejemplo 3: cierreMinimo([eslabon(c, b), eslabon(c, d), eslabon(a, b), eslabon(e, b), eslabon(d, b), eslabon(a, e)], Min).
 % Ejemplo 4: cierreMinimo([eslabon(a,b), eslabon(b,c), eslabon(c,a)], Min).
+% Ejemplo 5: cierreUnico([eslabon(c,b), eslabon(c,d), eslabon(a,b), eslabon(e,b), eslabon(d,b), eslabon(a,e)], Cierre).
+% Ejemplo 6: cierreUnico([eslabon(a,b), eslabon(b,c), eslabon(c,d), eslabon(d, a)], Cierre).
 
 alumno_prode(lopez, merlin, jaime, t110296).
 alumno_prode(copado, redondo, sergio, t110040).
@@ -77,3 +79,43 @@ findMin([Elem1, Elem2 | T], Min) :-
     length(Elem2, L2),
     L1 >= L2,
     findMin([Elem2 | T], Min).
+
+cierreUnico(OriginalList, Cierre) :-
+    findall(X, cierre(OriginalList, X), Result),
+    findUnique(Result, Cierre).
+
+findUnique([], Cierre).
+findUnique(_, Cierre) :-
+    ground(Cierre).
+findUnique([Elem | T], Cierre) :-
+    recursive(Elem, T, Cierre),
+    findUnique(T, Cierre).
+
+recursive(_, _, Cierre) :-
+    ground(Cierre).
+recursive(Elem, [], Cierre).
+recursive(Elem, [A | T], Cierre):-
+    length(Elem, L1),
+    length(A, L2),
+    L1 == L2,
+    is_sublist(Elem, A),
+    recursive(Elem,  T, Cierre).
+recursive(Elem, [A | T], Cierre):-
+    length(Elem, L1),
+    length(A, L2),
+    L1 == L2,
+    \+ is_sublist(Elem, A),
+    !,
+    Elem = Cierre,
+    recursive(Elem, T, Cierre).
+recursive(Elem, [A | T], Cierre):-
+    length(Elem, L1),
+    length(A, L2),
+    L1 =\= L2,
+    recursive(Elem,  T, Cierre).
+
+is_sublist([], List).
+is_sublist([X | T], List) :-
+    member(X, List),
+    delete(List, X, List1),
+    is_sublist(T, List1).
